9b84a775bfcb648501f95967780640c0aca54ad6
0xkf <Kanichi73@me.com>
2023-02-07 02:25:04 +0900

    first commit

---
+++ test_save.py
@@ -0,0 +1,25 @@
+import os
+from gpt_index import GPTSimpleVectorIndex, SimpleWebPageReader
+from dotenv import load_dotenv
+# env_path = os.path.join(os.path.dirname(__file__), '../.env')
+# load_dotenv(env_path)
+load_dotenv()
+
+MYAPI = os.getenv('MYAPI')
+THEURL = os.getenv('THEURL')
+
+print(MYAPI)
+print(THEURL)
+
+# QUESTION1 = os.getenv('QUESTION1')
+
+ 
+os.environ["OPENAI_API_KEY"] = MYAPI
+ 
+json_path = "./data/wiki.json"
+ 
+url = THEURL
+documents = SimpleWebPageReader(html_to_text=True).load_data([url])
+ 
+index = GPTSimpleVectorIndex(documents)
+index.save_to_disk(json_path)
